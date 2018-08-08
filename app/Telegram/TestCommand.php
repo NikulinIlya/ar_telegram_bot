173b0aa377a2602763c8b31937b4682f36dc5e74
Ilya <ilia.nikulin@gmail.com>
2018-08-08 09:21:19 +0300

    added keyboard

--- app/Telegram/TestCommand.php
+++ app/Telegram/TestCommand.php
@@ -26,17 +26,36 @@ class TestCommand extends Command
     public function handle()
     {
         $this->replyWithChatAction(['action' => Actions::TYPING]);
-//        $user = \App\User::find(1);
-//        $this->replyWithMessage(['text' => 'Почта пользователя в laravel: ' . $user->email]);
+        $user = \App\User::find(1);
+        $this->replyWithMessage(['text' => 'Почта пользователя в laravel: ' . $user->email]);
 
-//        $telegram_user = \Telegram::getWebhookUpdates()['message'];
-//        $text = sprintf('%s: %s'.PHP_EOL, 'Ваш номер чата', $telegram_user['from']['id']);
+        $telegram_user = \Telegram::getWebhookUpdates()['message'];
+        $text = sprintf('%s: %s'.PHP_EOL, 'Ваш номер чата', $telegram_user['from']['id']);
+
+        $keyboard = [
+            ['7', '8', '9'],
+            ['4', '5', '6'],
+            ['1', '2', '3'],
+            ['0']
+        ];
+
+        $reply_markup = \Telegram::replyKeyboardMarkup([
+            'keyboard' => $keyboard,
+            'resize_keyboard' => true,
+            'one_time_keyboard' => true
+        ]);
+
+        $response = \Telegram::sendMessage([
+            'chat_id' => 'CHAT_ID',
+            'text' => 'Hello World',
+            'reply_markup' => $reply_markup
+        ]);
+
+        $messageId = $response->getMessageId();
 
 //        if(!empty($telegram_user['from']['username'])) {
 //        $text .= sprintf('%s: %s'.PHP_EOL, 'Ваше имя пользователя в телеграм', $telegram_user['from']['username']);
-
-
-        $this->replyWithMessage('Hello!');
-//        $this->replyWithMessage(compact('text'));
+        
+        $this->replyWithMessage(compact('text'));
     }
 }
