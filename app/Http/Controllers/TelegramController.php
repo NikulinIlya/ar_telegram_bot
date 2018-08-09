9b3c1ddba3c085fe186f19d72877fcc65ecc2894
Ilya <ilia.nikulin@gmail.com>
2018-08-09 11:54:20 +0300

    url fix

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -33,7 +33,7 @@ class TelegramController extends Controller
 
     public function setWebHook()
     {
-        $url = 'https://applicationdomain.com/' . env('TELEGRAM_BOT_TOKEN') . '/webhook';
+        $url = env('APP_URL_H') . env('TELEGRAM_BOT_TOKEN') . '/webhook';
         $response = $this->telegram->setWebhook(['url' => $url]);
 
         return $response == true ? redirect()->back() : dd($response);
