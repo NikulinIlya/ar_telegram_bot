a5b6a182087d9127958f4be2528328ba13d7f034
Ilya <ilia.nikulin@gmail.com>
2018-08-09 13:44:52 +0300

    Functions

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -23,9 +23,7 @@
             <li class="nav-item">
                 <a class="nav-link" href="{{ url('/send-photo') }}">Send Photo</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ url('/') }}">GitHub Code</a>
-            </li>
+
         </ul>
     </div>
 </nav>
